b517c4dd8ca78eec64197b096dc2e4b70f6afb07
unknown <habeeb302@yahoo.com>
2019-06-03 12:16:26 +0800

    dynamo load data

--- app-data/dynamodbload.py
+++ app-data/dynamodbload.py
@@ -2,9 +2,15 @@ import boto3
 import json
 import decimal
 import datetime
+import csv
 
-dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
-print(dynamodb)
+dynamodb = boto3.resource('dynamodb')
+s3 = boto3.client('s3')
+
+obj = s3.get_object(Bucket='featuresample',Key='sample_data3.csv')
+rows1 = obj['Body'].read().decode("utf-8")
+rows = rows1.split('\n')
+
 
 table = dynamodb.Table('feature-value-dynamodb')
 
@@ -16,12 +22,19 @@ table = dynamodb.Table('feature-value-dynamodb')
 #        info = movie['info']
 
 print("Adding features:")
+with table.batch_writer() as batch:
 
-table.put_item(
-    Item={
-        'entity_id': 'user1',
-        'feature_name': 'feature1',
-        'value': '10',
-        'updated_time' : '2019-05-31'
-        }
-    )
+  for row in rows[1:]:
+      print(row)
+      entity_id = row.split(',')[0]
+      feature_name = row.split(',')[2]
+      value = row.split(',')[3]
+      updated_time = row.split(',')[1]
+      batch.put_item(
+       Item={
+        'entity_id': entity_id,
+        'feature_name': feature_name,
+        'value': value,
+        'updated_time' : updated_time
+             }
+      )
